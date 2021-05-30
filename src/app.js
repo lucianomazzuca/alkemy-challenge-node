e0e2a305881bcd24ab22353c69276d45592c2bec
Luciano Mazzuca <mazzucaluciano@hotmail.com>
2021-05-29 21:42:27 -0300

    Add sequelize first migrations and config files

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 require("dotenv").config();
 const express = require("express");
-const sequelize = require('./config/sequelize');
 
 const app = express();
 const port = 3000;
@@ -9,16 +8,6 @@ app.get("/", (req, res) => {
   res.send("Hello world");
 });
 
-async function testSequelize() {
-  try {
-    await sequelize.authenticate();
-    console.log("Connection has been established successfully.");
-  } catch (error) {
-    console.error("Unable to connect to the database:", error);
-  }
-}
-
-// testSequelize()
 
 app.listen(port, () => {
   // eslint-disable-next-line no-console
