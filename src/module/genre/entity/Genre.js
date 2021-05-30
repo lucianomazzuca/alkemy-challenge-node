115864a0aa6ccb3d434d77443140c755baccf111
Luciano Mazzuca <mazzucaluciano@hotmail.com>
2021-05-29 21:52:18 -0300

    Add Genre entity

--- src/module/genre/entity/Genre.js
+++ src/module/genre/entity/Genre.js
@@ -1,18 +1,12 @@
-module.exports = class Character {
+module.exports = class Genre {
   constructor({
     id,
     name,
-    age,
-    weight,
-    story,
     image,
     movies = []
   }) {
     this.id = id;
     this.name = name;
-    this.age = age;
-    this.weight = weight;
-    this.story = story;
     this.image = image;
     this.movies = movies;
   }
