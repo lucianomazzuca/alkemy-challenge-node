51bd48477e478a35e06657026c397f6c216b8cb5
Luciano Mazzuca <mazzucaluciano@hotmail.com>
2021-05-30 20:53:08 -0300

    Add tests for save method from genre repository

--- src/module/genre/repository/__test__/genreRepository.test.js
+++ src/module/genre/repository/__test__/genreRepository.test.js
@@ -1,26 +1,65 @@
-const { assert } = require("assert");
+/* eslint-disable no-unused-expressions */
 const { expect } = require("chai");
+
 const { Sequelize } = require("sequelize");
 
-const MovieModel = require('../../../movie/model/movieModel');
 const GenreModel = require("../../model/genreModel");
-const MovieGenreModel = require('../../../movie/model/movieGenreModel');
 const GenreRepository = require("../genreRepository");
 
 describe("Genre repository methods", () => {
+  let genreRepository;
+  let genreModel;
+
   beforeEach(async () => {
     // Setup DB in memory
-    const sequelizeInstance = new Sequelize("sqlite::memory", { logging: false });
+    const sequelizeInstance = new Sequelize("sqlite::memory", {
+      logging: false,
+    });
 
     // Setup Models
-    const genreModel = GenreModel.setup(sequelizeInstance);
+    genreModel = GenreModel.setup(sequelizeInstance);
 
     // Instantiate repository
-    const genreRepository = new GenreRepository(genreModel);
+    genreRepository = new GenreRepository(genreModel);
 
     await sequelizeInstance.sync({ force: true });
-
   });
 
-  it("save should create a new genre", async () => {});
+  describe("Save method", () => {
+    it("should add a new genre to the db", async () => {
+      const genre = {
+        id: undefined,
+        name: "Action",
+        image: "test",
+      };
+
+      await genreRepository.save(genre);
+
+      const savedGenre = await genreModel.findByPk(1);
+      expect(savedGenre.id).to.equal(1);
+      expect(savedGenre.name).to.equal("Action");
+    });
+
+    it("should update an existing genre in the db", async () => {
+      const genre = {
+        id: undefined,
+        name: "Action",
+        image: "test",
+      };
+
+      await genreRepository.save(genre);
+
+      const updatedGenre = {
+        id: 1,
+        name: "Adventure",
+        image: "test",
+      };
+
+      await genreRepository.save(updatedGenre);
+
+      const savedGenre = await genreModel.findByPk(1);
+      expect(savedGenre.id).to.equal(1);
+      expect(savedGenre.name).to.equal("Adventure");
+    });
+  });
 });
