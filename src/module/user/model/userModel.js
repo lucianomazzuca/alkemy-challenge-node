0c9ad0e3abb00f909ac5c7f6b5ec614d48d08ef7
Luciano Mazzuca <mazzucaluciano@hotmail.com>
2021-06-05 03:14:22 -0300

    Add user model

---
+++ src/module/user/model/userModel.js
@@ -0,0 +1,34 @@
+const { DataTypes, Model } = require("sequelize");
+
+class User extends Model {
+  static setup(sequelizeInstance) {
+    User.init(
+      {
+        id: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+        },
+        name: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        password: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+      },
+      {
+        sequelize: sequelizeInstance,
+        modelName: "User",
+        tableName: "users",
+        underscored: true,
+      }
+    );
+
+    return User;
+  }
+}
+
+module.exports = User;
