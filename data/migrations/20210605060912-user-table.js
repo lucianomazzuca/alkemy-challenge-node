6fcc612689cc733a53ab632a4748cf638b456601
Luciano Mazzuca <mazzucaluciano@hotmail.com>
2021-06-05 03:12:15 -0300

    Add user table migration

---
+++ data/migrations/20210605060912-user-table.js
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("users", {
+      id: {
+        type: Sequelize.DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false,
+      },
+      created_at: Sequelize.DATE,
+      updated_at: Sequelize.DATE,
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("users");
+  }
+};
---
+++ src/module/genre/service/genreService.js
@@ -0,0 +1,1 @@
+const Genre = require('../entity/Genre');
