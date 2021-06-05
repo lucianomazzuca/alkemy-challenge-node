ba47d4cc4b758acf6a76aff52f1f2e140ca7f6b0
Luciano Mazzuca <mazzucaluciano@hotmail.com>
2021-06-05 03:34:18 -0300

    Add getByName method to user repository

--- src/module/user/repository/__test__/userRepository.test.js
+++ src/module/user/repository/__test__/userRepository.test.js
@@ -8,6 +8,7 @@ const UserModel = require("../../model/userModel");
 const UserRepository = require("../userRepository");
 
 const createUserTest = require("../../fixture/userFixture");
+const NotFoundError = require("../../../../shared/error/NotFoundError");
 
 describe("User repository methods", () => {
   let userModel;
@@ -42,10 +43,41 @@ describe("User repository methods", () => {
       user.name = "Test2";
 
       await userRepository.save(user);
-      
+
       const userInDb = await userModel.findByPk(1);
       expect(userInDb.id).to.equal(1);
-      expect(userInDb.name).to.equal('Test2');
+      expect(userInDb.name).to.equal("Test2");
+    });
+  });
+
+  describe("getById method", () => {
+    it("should return the first user in db", async () => {
+      const user = createUserTest();
+      await userRepository.save(user);
+
+      const userInDb = await userRepository.getById(1);
+      expect(userInDb.id).to.equal(1);
+      expect(userInDb.name).to.equal(user.name);
+    });
+
+    it("should throw error when an user is not found", async () => {
+      await expect(userRepository.getById(1)).to.be.rejectedWith(NotFoundError);
+    });
+  });
+
+  describe("getByName method", () => {
+    it("should return the first user in db", async () => {
+      const user = createUserTest();
+      await userRepository.save(user);
+
+      const userInDb = await userRepository.getByName(user.name);
+      expect(userInDb.id).to.equal(1);
+      expect(userInDb.name).to.equal(user.name);
+    });
+
+    it("should return null if the user is not found", async () => {
+      const userInDb = await userRepository.getByName('test');
+      expect(userInDb).to.be.null;
     });
   });
 });
--- src/module/user/repository/userRepository.js
+++ src/module/user/repository/userRepository.js
@@ -1,3 +1,4 @@
+const NotFountError = require("../../../shared/error/NotFoundError");
 const { fromModelToEntity } = require("../mapper/userMapper");
 
 module.exports = class UserRepository {
@@ -13,5 +14,24 @@ module.exports = class UserRepository {
     await newUser.save();
 
     return fromModelToEntity(newUser);
+  }
+
+  async getById(id) {
+    const user = await this.userModel.findByPk(id);
+    if (!user) {
+      throw new NotFountError(`User with id ${id} was not found`);
+    }
+
+    return fromModelToEntity(user);
+  }
+
+  async getByName(name) {
+    const user = await this.userModel.findOne({ where: { name } });
+
+    if (!user) {
+      return null;
+    }
+
+    return fromModelToEntity(user);
   }
 };
