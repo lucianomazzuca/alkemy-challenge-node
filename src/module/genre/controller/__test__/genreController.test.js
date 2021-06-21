00f84ca1265c35c63686c6599ac1e01336b23e21
Luciano Mazzuca <mazzucaluciano@hotmail.com>
2021-06-21 19:48:53 -0300

    Add genre controller

---
+++ src/module/genre/controller/__test__/genreController.test.js
@@ -0,0 +1,119 @@
+const chai = require("chai");
+
+const { expect } = chai;
+const sinon = require("sinon");
+
+const GenreController = require("../genreController");
+
+const createGenreTest = require("../../fixture/genreFixture");
+const NotFoundError = require("../../../../shared/error/NotFoundError");
+
+const mockGenreService = {
+  save: sinon.stub(),
+  getAll: sinon.stub(),
+  delete: sinon.stub(),
+  getById: sinon.stub(),
+};
+
+const genreController = new GenreController(mockGenreService);
+
+const reqMock = {
+  body: createGenreTest(),
+  params: {
+    id: 1,
+  },
+};
+const resMock = {
+  json: sinon.stub(),
+  sendStatus: sinon.spy(),
+  status: sinon.spy(() => resMock),
+};
+const nextMock = sinon.mock();
+
+describe("Genre controller methods", () => {
+  afterEach(() => {
+    sinon.reset();
+  });
+
+  describe("create method", async () => {
+    it("should call service's save method", async () => {
+      const genre = createGenreTest();
+
+      await genreController.create(reqMock, resMock, nextMock);
+
+      expect(mockGenreService.save.calledOnceWith(genre)).to.be.true;
+      expect(resMock.sendStatus.calledOnceWith(201)).to.be.true;
+    });
+  });
+
+  describe("getAll method", async () => {
+    it("should call service's getAll method and send genres as json", async () => {
+      const genres = [createGenreTest(), createGenreTest()];
+      mockGenreService.getAll.returns(genres);
+
+      await genreController.getAll(reqMock, resMock, nextMock);
+
+      expect(mockGenreService.getAll.calledOnce).to.be.true;
+      expect(resMock.status.calledOnceWith(200)).to.be.true;
+      expect(resMock.json.calledOnceWith(genres)).to.be.true;
+    });
+  });
+
+  describe("getById method", async () => {
+    it("should send a status 200 and a genre as json", async () => {
+      const genre = createGenreTest();
+      mockGenreService.getById.returns(genre);
+
+      await genreController.getById(reqMock, resMock, nextMock);
+
+      expect(resMock.status.calledOnceWith(200)).to.be.true;
+      expect(resMock.json.calledOnceWith(genre)).to.be.true;
+    });
+
+    it("should send a status 401 when getById throws an error", async () => {
+      mockGenreService.getById.onFirstCall().throws(new NotFoundError());
+
+      await genreController.getById(reqMock, resMock, nextMock);
+      expect(resMock.status.calledOnceWith(401)).to.be.true;
+    });
+  });
+
+  describe("delete method", async () => {
+    it("should call service's delete method and send a status 200", async () => {
+      await genreController.delete(reqMock, resMock, nextMock);
+
+      expect(mockGenreService.delete.calledOnceWith(1)).to.be.true;
+      expect(resMock.sendStatus.calledOnceWith(200)).to.be.true;
+    });
+
+    it("should send a status 401 when getById throws an error", async () => {
+      mockGenreService.getById
+        .onFirstCall()
+        .throws(() => new NotFoundError());
+
+      await genreController.delete(reqMock, resMock, nextMock);
+      expect(resMock.status.calledOnceWith(401)).to.be.true;
+    });
+  });
+
+  describe("edit method", async () => {
+    it("should call service's save method and send a status 200", async () => {
+      const genre = createGenreTest();
+      genre.id = 1;
+
+      await genreController.edit(reqMock, resMock, nextMock);
+
+      expect(mockGenreService.save.calledOnceWith(genre)).to.be.true;
+      expect(resMock.sendStatus.calledOnceWith(200)).to.be.true;
+    });
+
+    it("should send a status 401 when getById throws an error", async () => {
+      mockGenreService.getById
+        .onFirstCall()
+        .throws(() => new NotFoundError());
+
+      await genreController.edit(reqMock, resMock, nextMock);
+      expect(resMock.status.calledOnceWith(401)).to.be.true;
+    });
+  });
+});
---
+++ src/module/genre/controller/genreController.js
@@ -0,0 +1,83 @@
+const NotFoundError = require("../../../shared/error/NotFoundError");
+
+module.exports = class GenreController {
+  constructor(genreService) {
+    this.genreService = genreService;
+  }
+
+  async create(req, res, next) {
+    // Map image filename to genre
+    if (req.file) {
+      req.body.image = req.file.filename;
+    }
+
+    const genre = req.body;
+
+    try {
+      await this.genreService.save(genre);
+    } catch (e) {
+      next(e);
+    }
+
+    return res.sendStatus(201);
+  }
+
+  async getAll(req, res, next) {
+    const params = req.query;
+    try {
+      const genres = await this.genreService.getAll(params);
+      res.status(200).json(genres);
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  async getById(req, res, next) {
+    try {
+      const genre = await this.genreService.getById(req.params.id);
+      return res.status(200).json(genre);
+    } catch (e) {
+      if (e instanceof NotFoundError) {
+        return res.status(401).json({ error: e.message });
+      }
+      return next(e);
+    }
+  }
+
+  async delete(req, res, next) {
+    try {
+      // check if genre exists
+      await this.genreService.getById(req.params.id);
+
+      await this.genreService.delete(req.params.id);
+      return res.sendStatus(200);
+    } catch (e) {
+      if (e instanceof NotFoundError) {
+        return res.status(401).json({ error: e.message });
+      }
+      return next(e);
+    }
+  }
+
+  async edit(req, res, next) {
+    if (req.file) {
+      req.body.image = req.file.filename;
+    }
+
+    const genre = req.body;
+    genre.id = req.params.id;
+
+    try {
+      // check if genre exists
+      await this.genreService.getById(req.params.id);
+
+      await this.genreService.save(genre);
+      return res.sendStatus(200);
+    } catch (e) {
+      if (e instanceof NotFoundError) {
+        return res.status(401).json({ error: e.message });
+      }
+      return next(e);
+    }
+  }
+};
