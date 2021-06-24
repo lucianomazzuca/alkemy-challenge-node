e4d8451af0b3d6c78842ae497c8eee6af836b538
Luciano Mazzuca <mazzucaluciano@hotmail.com>
2021-06-24 00:08:37 -0300

    Add validateCharacters method to character service

--- src/module/character/service/__test__/characterService.test.js
+++ src/module/character/service/__test__/characterService.test.js
@@ -11,7 +11,7 @@ const mockCharacterRepository = {
   save: sinon.stub(),
   getAll: sinon.stub(),
   delete: sinon.spy(),
-  getById: sinon.spy(),
+  getById: sinon.stub(),
 };
 
 const characterService = new CharacterService(mockCharacterRepository);
@@ -52,6 +52,25 @@ describe("Character Service methods", () => {
       await characterService.getById(1);
 
       expect(mockCharacterRepository.getById.calledOnceWith(1)).to.be.true;
+    });
+  });
+
+  describe("validateCharacters method", () => {
+    it("should call getById method and return an empty array", async () => {
+      const character = createCharacterTest();
+      mockCharacterRepository.getById.returns(character);
+
+      const result = await characterService.validateCharacters([1, 2, 3]);
+      expect(result).to.be.an("array").and.have.lengthOf(0);
+    });
+
+    it("should return an array with error messages when character doesn't exist", async () => {
+      mockCharacterRepository.getById.returns(null);
+
+      const result = await characterService.validateCharacters([1, 2]);
+      expect(result).to.be.an("array").and.have.lengthOf(2);
+      expect(result[0]).to.equal("Character with id 1 doesn't exist");
+      expect(result[1]).to.equal("Character with id 2 doesn't exist");
     });
   });
 });
--- src/module/character/service/characterService.js
+++ src/module/character/service/characterService.js
@@ -1,7 +1,7 @@
 module.exports = class CharacterService {
   constructor(characterRepository) {
     this.characterRepository = characterRepository;
-  };
+  }
 
   async save(character) {
     return this.characterRepository.save(character);
@@ -9,21 +9,32 @@ module.exports = class CharacterService {
 
   async getAll(params) {
     const characters = await this.characterRepository.getAll(params);
-    const charactersNameAndImage = characters.map(character => (
-      {
-        name: character.name,
-        image: character.image
-      }
-    ))
+    const charactersNameAndImage = characters.map((character) => ({
+      name: character.name,
+      image: character.image,
+    }));
 
     return charactersNameAndImage;
   }
 
   async delete(id) {
     return this.characterRepository.delete(id);
-  };
+  }
 
   async getById(id) {
     return this.characterRepository.getById(id);
   }
-}
+
+  async validateCharacters(charactersId) {
+    const errors = [];
+
+    charactersId.forEach(async (id) => {
+      const character = await this.getById(id);
+      if (!character) {
+        errors.push(`Character with id ${id} doesn't exist`);
+      }
+    });
+
+    return errors;
+  }
+};
